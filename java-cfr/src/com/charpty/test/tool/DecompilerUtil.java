f8e4bac2d1779e0906ce7afac49dda67e838f649
charpty <charpty@gmail.com>
2018-12-08 22:19:21 +0800

    修改：递归遍历目标目录

--- java-cfr/src/com/charpty/test/tool/DecompilerUtil.java
+++ java-cfr/src/com/charpty/test/tool/DecompilerUtil.java
@@ -1,9 +1,7 @@
 package com.charpty.test.tool;
 
 import java.io.File;
-import java.io.FilenameFilter;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 
 import org.benf.cfr.reader.state.ClassFileSourceImpl;
@@ -33,7 +31,8 @@ public class DecompilerUtil {
         GetOptParser optParser = new GetOptParser();
         Options options = null;
 
-        List<File> files = getVaildFiles(classDir);
+        List<File> files = new ArrayList<>(128);
+        getVaildFiles(classDir, files);
         System.out.println("-------------------------------list files-------------------------------");
         for (File f : files) {
             System.out.println(f.getName() + " : " + f.getAbsolutePath());
@@ -71,7 +70,7 @@ public class DecompilerUtil {
         }
     }
 
-    private static List<File> getVaildFiles(String baseDir) {
+    private static void getVaildFiles(String baseDir, List<File> result) {
         if (baseDir == null || baseDir.isEmpty()) {
             throw new UtilException("baseDir can not be null");
         }
@@ -79,26 +78,24 @@ public class DecompilerUtil {
         if (!tmp.exists()) {
             throw new UtilException("no such file or directory: " + baseDir);
         }
-        List<File> result = new ArrayList<File>();
-        if (tmp.isDirectory()) {
-            File[] files = tmp.listFiles(new FilenameFilter() {
-                @Override
-                public boolean accept(File dir, String name) {
-                    // 如果是jar文件或者class文件则直接返回true
-                    if (name.endsWith(".jar") || name.endsWith(".class")) {
-                        return true;
-                    }
-                    return false;
-                }
-            });
-            result.addAll(Arrays.asList(files));
-        } else {
+        if (!tmp.isDirectory()) {
             if (baseDir.endsWith(".jar") || baseDir.endsWith(".class")) {
                 result.add(tmp);
             } else {
                 throw new UtilException("single file must be *.jar or *.class: " + baseDir);
             }
+            return;
         }
-        return result;
+
+        File[] files = tmp.listFiles();
+        for (File f : files) {
+            String path = f.getPath();
+            if (f.isDirectory()) {
+                getVaildFiles(path, result);
+            }
+            if (path.endsWith(".jar") || path.endsWith(".class")) {
+                result.add(f);
+            }
+        }
     }
 }
